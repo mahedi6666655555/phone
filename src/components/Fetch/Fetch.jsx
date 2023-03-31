93213984b78917123a3f58d61cd8c9732577fde4
mahedi6666655555 <mahedinir34678@gmail.com>
2023-03-31 22:09:00 +0600

    website is done

--- src/components/Fetch/Fetch.jsx
+++ src/components/Fetch/Fetch.jsx
@@ -5,16 +5,34 @@ const Fetch = () => {
   let [api, setApi] = useState([]);
 
   useEffect(() => {
-    let url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    let url = `https://openapi.programming-hero.com/api/phones?search=samsung`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setApi(data.data));
   }, []);
 
-  console.log(api);
+  let [total2, setTotal] = useState([]);
+  let total = () => {
+    let get1 = document.getElementById("inm");
+    let final = get1.value;
+    setTotal(final);
+  };
+
   return (
     <div>
-      <div className=" mt-5 w-11/12  mx-auto gap-5 grid grid-cols-4">
+      <div className="flex ">
+        <input
+          type="text"
+          id="inm"
+          className=" ml-5 mt-5 w-[300px] h-7 mb-5 bg-gray-400"
+        />
+        <div>
+          <button onClick={total} className="mt-5 ml-5">
+            search
+          </button>
+        </div>
+      </div>
+      <div className=" mt-5 w-11/12  mx-auto gap-5 grid md:grid-cols-4">
         {api.map((res) => (
           <Single key={res.slug} Single={res}></Single>
         ))}
--- src/components/single/Single.jsx
+++ src/components/single/Single.jsx
@@ -3,6 +3,11 @@ import React from "react";
 const Single = (props) => {
   let { brand, image, phone_name } = props.Single;
   //   console.log(phone_name);
+
+  let mahedi = () => {
+    console.log("all ok");
+  };
+
   return (
     <div>
       <div className="w-[300px] h-[500px] border-2 rounded-lg shadow-lg p-5">
@@ -12,7 +17,9 @@ const Single = (props) => {
         <h6 className="text-lg font-bold pt-3 pb-2">{brand}</h6>
         <h1 className="text-lg font-bold pb-2">{phone_name}</h1>
         <div>
-            <button className="border-b-2">buy now</button>
+          <button onClick={mahedi} className="border-b-2">
+            buy now
+          </button>
         </div>
       </div>
     </div>
