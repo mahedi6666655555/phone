ec13220c0a08ccad98321e96bc187fe5cda197f3
mahedi6666655555 <mahedinir34678@gmail.com>
2023-03-31 22:13:02 +0600

    react tost add done

--- src/components/single/Single.jsx
+++ src/components/single/Single.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const Single = (props) => {
   let { brand, image, phone_name } = props.Single;
   //   console.log(phone_name);
 
   let mahedi = () => {
-    console.log("all ok");
+    toast("Wow so easy!");
   };
 
   return (
@@ -18,10 +19,11 @@ const Single = (props) => {
         <h1 className="text-lg font-bold pb-2">{phone_name}</h1>
         <div>
           <button onClick={mahedi} className="border-b-2">
-            buy now
+            add to cart
           </button>
         </div>
       </div>
+      <ToastContainer></ToastContainer>
     </div>
   );
 };
